4f38565172214bd580de5ba20d3a1c9a5e05f2fa
izmi.komar <komar.izmi@gmail.com>
2024-11-21 21:20:29 +0700

    implement roles service methods; add getAll, findOne, store, update, and destroy methods with corresponding schemas

--- src/api/roles/roles.service.ts
+++ src/api/roles/roles.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { RolesRepository } from './roles.repository';
+import { RolesCreateSchema, RolesQuerySchema } from './roles.schema';
 
 @Injectable()
 export class RolesService {
   constructor(private readonly rolesRepo: RolesRepository) {}
+
+  async getAll(query: RolesQuerySchema) {}
+  async findOne(id: string) {}
+  async store(data: RolesCreateSchema) {}
+  async update(id: string, data: RolesCreateSchema) {}
+  async destroy(id: string) {}
 }
