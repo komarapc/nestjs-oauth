948156b4e7a8e184cbfba325b4a18f3fb9bb5afa
izmi.komar <komar.izmi@gmail.com>
2024-11-21 21:24:16 +0700

    implement roles controller; add CRUD endpoints for roles management

--- src/api/roles/roles.controller.ts
+++ src/api/roles/roles.controller.ts
@@ -1,7 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
+import { RolesCreateSchema, RolesQuerySchema } from './roles.schema';
+import { Response } from 'express';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
+
+  @Get()
+  async getAll(@Query() query: RolesQuerySchema, @Res() res: Response) {
+    const result = await this.rolesService.getAll(query);
+    res.status(HttpStatus.OK).json(result);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Res() res: Response) {
+    const result = await this.rolesService.findOne(id);
+    res.status(HttpStatus.OK).json(result);
+  }
+
+  @Post()
+  async store(@Body() body: RolesCreateSchema, @Res() res: Response) {
+    const result = await this.rolesService.store(body);
+    res.status(HttpStatus.CREATED).json(result);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() body: RolesCreateSchema,
+    @Res() res: Response,
+  ) {
+    const result = await this.rolesService.update(id, body);
+    res.status(HttpStatus.OK).json(result);
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id') id: string, @Res() res: Response) {
+    const result = await this.rolesService.destroy(id);
+    res.status(HttpStatus.OK).json(result);
+  }
 }
